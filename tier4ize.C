7f94ea29ce7edb32a7b2b9baeeaeb7d9c8b2c31c
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2020-05-14 23:10:36 +0200

    Save number of primaries in tier4ized files

--- tier4ize.C
+++ tier4ize.C
@@ -1,55 +1,100 @@
+#include <dirent.h>
+
 using namespace gada;
+
+// get raw-*.root files in directory
+std::vector<std::string> readdir(std::string foldName) {
+
+    std::vector<std::string> filelist;
+    auto p = std::unique_ptr<DIR,std::function<int(DIR*)>>{opendir(foldName.c_str()), &closedir};
+    if (!p) { std::cerr << "ERROR: invalid or empty directory path!\n"; return filelist; }
+    dirent entry;
+    for (auto* r = &entry; readdir_r(p.get(), &entry, &r) == 0 && r; ) {
+        // this means "is a regular file", DT_REG = 8
+        if (entry.d_type == 8 &&
+            std::string(entry.d_name).find(".root") != std::string::npos) {
+            filelist.push_back(foldName + "/" + std::string(entry.d_name));
+        }
+    }
+    std::sort(filelist.begin(), filelist.end());
+    std::cout << "sorted ROOT files:\n";
+    for (const auto & f : filelist) std::cout << "  " << f << std::endl;
+    return filelist;
+};
 
 void tier4ize(std::string dir, int run_id, std::string outname) {
 
-    std::string kMappingFile;
-    std::string kResolutionFile;
-    std::string kEnergyReconstruction;
-    std::string kTransitionsFile;
-    std::string kGeometryFile;
+    std::string kMappingFile          = "meta/mapping-spmMerged.json";
+    std::string kResolutionFile       = "meta/ged-resolution-super-calib.json";
+    std::string kEnergyReconstruction = "Zac";
+    std::string kTransitionsFile      = "meta/ged-transition-linear-bjorn.json";
+    std::string kGeometryFile         = "meta/ged-parameters.json";
     std::string kGedSettingsFile;
     ULong64_t kTimestamp;
 
     switch (run_id) {
         case 76 : {
-            kMappingFile          = "meta/mapping-spmMerged.json";
-            kResolutionFile       = "meta/ged-resolution-super-calib.json";
-            kEnergyReconstruction = "Zac";
-            kTransitionsFile      = "meta/ged-transition-linear-bjorn.json";
-            kGeometryFile         = "meta/ged-parameters.json";
             kGedSettingsFile      = "meta/ged-settings-run76pca.json";
             kTimestamp            = 1486293301;
             break;
         }
         case 68 : {
-            kMappingFile          = "meta/mapping-spmMerged.json";
-            kResolutionFile       = "meta/ged-resolution-super-calib.json";
-            kEnergyReconstruction = "Zac";
-            kTransitionsFile      = "meta/ged-transition-linear-bjorn.json";
-            kGeometryFile         = "meta/ged-parameters.json";
             kGedSettingsFile      = "meta/ged-settings-run68pca.json";
             kTimestamp            = 1468229805;
             break;
         }
     }
 
+    std::cout << "Mapping                -> " << kMappingFile          << std::endl;
+    std::cout << "Resolutions            -> " << kResolutionFile       << std::endl;
+    std::cout << "Energy                 -> " << kEnergyReconstruction << std::endl;
+    std::cout << "Transition/Dead layers -> " << kTransitionsFile      << std::endl;
+    std::cout << "GeDet geometries       -> " << kGeometryFile         << std::endl;
+    std::cout << "GeDet settings         -> " << kGedSettingsFile      << std::endl;
+    std::cout << "Timestamp              -> " << kTimestamp            << std::endl;
+
+    Long64_t primaries = 0;
+
+    auto filelist = readdir(dir);
+    if (filelist.empty()) {
+        std::cerr << "ERROR: no ROOT files found in '" << dir << "', aborting" << std::endl;
+        return;
+    }
+
+    TFile* tf;
+    for (auto f : filelist) {
+        tf = TFile::Open(f.c_str());
+        auto obj = dynamic_cast<TParameter<long>*>(tf->Get("NumberOfPrimaries"));
+        if (!obj) {
+            std::cerr << "ERROR: could not find NumberOfPrimaries object in file '"
+                      << f << "', aborting" << std::endl;
+            return;
+        }
+        primaries += obj->GetVal();
+        tf->Close();
+    }
+
     setenv("MU_CAL", "meta/gerda-metadata/config/_aux/geruncfg", 1);
 
-    TChain* sim = new TChain("fTree");
-    sim->Add((dir + "/*.root").c_str());
+    TChain sim("fTree");
+    sim.Add((dir + "/*.root").c_str());
 
-    T4SimConfig* config = new T4SimConfig();
-    config->LoadMapping(kMappingFile);
-    config->LoadGedSettings(kGedSettingsFile);
-    config->LoadGedResolutions(kResolutionFile, kEnergyReconstruction);
-    config->LoadGedTransitions(kTransitionsFile, kGeometryFile);
-    config->LoadRunConfig(kTimestamp);
+    T4SimConfig config;
+    config.LoadMapping(kMappingFile);
+    config.LoadGedSettings(kGedSettingsFile);
+    config.LoadGedResolutions(kResolutionFile, kEnergyReconstruction);
+    config.LoadGedTransitions(kTransitionsFile, kGeometryFile);
+    config.LoadRunConfig(kTimestamp);
 
-    T4SimHandler* handler = new T4SimHandler(sim, config, outname);
-    handler->SetBranchAddresses();
-    handler->RunProduction();
+    T4SimHandler handler(&sim, &config, outname);
+    handler.SetBranchAddresses();
+    handler.RunProduction();
 
-    delete handler;
-    delete config;
-    delete sim;
+    // save number of primaries
+    TFile fout(outname.c_str(), "UPDATE");
+    TParameter<Long64_t> NumberOfPrimaries("NumberOfPrimaries", primaries);
+    NumberOfPrimaries.Write();
+    fout.Close();
+
+    return;
 }
